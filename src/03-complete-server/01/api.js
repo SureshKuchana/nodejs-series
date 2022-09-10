c115fd671f347ac93c1b77007c53f34d098b0090
SureshKuchana <kuchana123.suresh@gmail.com>
2022-09-10 13:33:18 +0530

    added product.json & filter products

---
+++ src/03-complete-server/01/api.js
@@ -0,0 +1,24 @@
+const Products = require("./products");
+
+// make sure, the fuction declaration should be the normal function declaration
+// Don't use the ES6 const to declare the fun, it will throw error,
+// ReferenceError: Cannot access 'listProducts' before initialization
+async function listProducts(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const { offset = 0, limit = 25, tag } = req.query;
+  try {
+    res.json(
+      await Products.list({
+        offset: Number(offset),
+        limit: Number(limit),
+        tag,
+      })
+    );
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+module.exports = {
+  listProducts,
+};
