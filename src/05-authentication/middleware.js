489d339856df79a65e283e2f8029128dca12bd41
SureshKuchana <kuchana123.suresh@gmail.com>
2022-09-18 11:00:55 +0530

    re-structured code

--- src/05-authentication/middleware.js
+++ src/05-authentication/middleware.js
@@ -1,33 +1,44 @@
+const { STATUS_CODES } = require("http");
+
 module.exports = {
   cors,
   notFound,
-  handleError
-}
+  handleError,
+  handleValidationError,
+};
 
-function cors (req, res, next) {
-  const origin = req.headers.origin
+function cors(req, res, next) {
+  const origin = req.headers.origin;
 
-  res.setHeader('Access-Control-Allow-Origin', origin || '*')
+  res.setHeader("Access-Control-Allow-Origin", origin || "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'POST, GET, PUT, DELETE, OPTIONS, XMODIFY'
-  )
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Max-Age', '86400')
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, DELETE, OPTIONS, XMODIFY"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "86400");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-  )
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
 
-  next()
+  next();
 }
 
-function handleError (err, req, res, next) {
-  console.error(err)
-  if (res.headersSent) return next(err)
-  res.status(500).json({ error: 'Internal Error' })
+function handleError(err, req, res, next) {
+  console.error(err);
+  if (res.headersSent) return next(err);
+  const statusCode = err.statusCode || 500;
+  const errorMessage = STATUS_CODES[statusCode] || "Internal Error";
+  res.status(statusCode).json({ error: errorMessage });
 }
 
-function notFound (req, res) {
-  res.status(404).json({ error: 'Not Found' })
+function notFound(req, res) {
+  res.status(404).json({ error: "Not Found" });
+}
+
+function handleValidationError(err, req, res, next) {
+  if (err.name !== "ValidationError") return next(err);
+
+  res.status(400).json({ error: err._message, errorDetails: err.errors });
 }
--- src/05-authentication/server.js
+++ src/05-authentication/server.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const os = require("os");
-const passport = require("passport");
-const Strategy = require("passport-local").Strategy;
-const expressSession = require("express-session");
 const cookieParser = require("cookie-parser");
+const os = require("os");
 
 const api = require("./api");
+const auth = require("./auth");
 const middleware = require("./middleware");
 
 const port = process.env.PORT || 1337;
-const sessionSecret = process.env.SESSION_SECRET || "session secret";
-const adminPassword = process.env.ADMIN_PASSWORD || "password";
-
-passport.use(
-  new Strategy((username, password, cb) => {
-    const isAdmin = username === "admin" && password === adminPassword;
-    if (isAdmin)
-      return cb(null, {
-        username: "admin",
-      });
-    cb(null, false);
-  })
-);
-
-passport.serializeUser((user, cb) => cb(null, user));
-passport.deserializeUser((user, cb) => cb(null, user));
 
 const app = express();
 const hostname = os.hostname();
@@ -33,43 +15,30 @@ const hostname = os.hostname();
 app.use(middleware.cors);
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(
-  expressSession({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+auth.setMiddleware(app);
 
 app.get("/", (req, res) => {
   res.json({ machine: hostname });
 });
 
-app.post("/login", passport.authenticate("local"), (req, res) =>
-  res.json({ success: true })
-);
-app.get("/products", ensureAdmin, api.listProducts);
-app.post("/products", api.createProduct);
-app.get("/products/:id", ensureAdmin, api.getProduct);
-app.put("/products/:id", ensureAdmin, api.editProduct);
-app.delete("/products/:id", ensureAdmin, api.deleteProduct);
+app.post("/login", auth.authenticate, auth.login);
 
+app.get("/products", auth.ensureAdmin, api.listProducts);
+app.post("/products", api.createProduct);
+app.get("/products/:id", auth.ensureAdmin, api.getProduct);
+app.put("/products/:id", auth.ensureAdmin, api.editProduct);
+app.delete("/products/:id", auth.ensureAdmin, api.deleteProduct);
+
+app.get("/orders", auth.ensureAdmin, api.listOrders);
+app.post("/orders", auth.ensureAdmin, api.createOrder);
+
+app.use(middleware.handleValidationError);
 app.use(middleware.handleError);
 app.use(middleware.notFound);
 
 const server = app.listen(port, () =>
   console.log(`Server listening on port http://localhost:${port}`)
 );
-
-function ensureAdmin(req, res, next) {
-  console.log(" req ", req.user);
-  const isAdmin = req.user && req.user.username === "admin";
-  if (isAdmin) return next();
-
-  res.status(401).json({ error: "Unauthorized" });
-}
 
 if (require.main !== module) {
   module.exports = server;
